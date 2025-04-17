9c86995520a8059ba9898a91c4996bc83df4f124
Adrian Mohnacs <amohnacs@gmail.com>
2025-04-17 11:21:13 +0700

    Successful candidate assign and loading and success modals

--- frontend/src/pages/campaigns/create-from-doc.tsx
+++ frontend/src/pages/campaigns/create-from-doc.tsx
@@ -740,7 +740,7 @@ const CreateCampaignFromDocPage = () => {
             </div>
             
             {/* Add Candidate Selection Section */}
-            {/* <div className="bg-white shadow sm:rounded-lg">
+            <div className="bg-white shadow sm:rounded-lg">
               <div className="px-4 py-5 sm:p-6">
                 <h3 className="text-lg font-medium leading-6 text-gray-900">Assign Candidates</h3>
                 <div className="mt-2 max-w-xl text-sm text-gray-500">
@@ -787,7 +787,7 @@ const CreateCampaignFromDocPage = () => {
                   )}
                 </div>
               </div>
-            </div> */}
+            </div>
             
             {/* Submit button */}
             <div className="pt-4">
